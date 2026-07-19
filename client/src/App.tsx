89f454154a3b1f9a13a6c578ef07f9e360188a52
barata <apsiitese@gmail.com>
2026-07-19 12:14:42 +1000

    Add Vercel Analytics for visitor tracking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "./components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/home";
 import ServicesPage from "./pages/services";
 import AboutPage from "./pages/about";
@@ -32,6 +33,7 @@ function App() {
       <TooltipProvider>
         <Toaster />
         <Router />
+        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   );
